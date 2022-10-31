79d028c13a23d2eef2ab385b5eb996f3fc28d9ed
anaalucca <107819322+anaalucca@users.noreply.github.com>
2022-10-31 17:57:34 -0300

    Add files via upload

---
+++ function_consulta_marcada.sql
@@ -0,0 +1,8 @@
+CREATE FUNCTION `consulta_marcada` (data DATE, hora VARCHAR (5), id INT)
+RETURNS VARCHAR (100)
+READS SQL DATA
+BEGIN
+	RETURN (SELECT CONCAT('', tipo_consulta, ', paciente ', nome_paciente, ', cliente ', nome_cliente)
+	FROM consulta c JOIN paciente p ON (c.id_paciente = p.id_paciente) JOIN cliente l ON (p.id_cliente = l.id_cliente)
+	WHERE data_consulta = data and hora_consulta = hora and id_mv = id);
+END 
