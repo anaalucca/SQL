2c771fc28b4af3d94efa701b200c41cbef52df28
anaalucca <107819322+anaalucca@users.noreply.github.com>
2022-10-31 17:58:24 -0300

    Update function_consulta_marcada.sql

--- function_consulta_marcada.sql
+++ function_consulta_marcada.sql
@@ -1,3 +1,5 @@
+-- Retorna o total do faturamento da clínica até o momento no banco de dados = 5975
+
 CREATE FUNCTION `consulta_marcada` (data DATE, hora VARCHAR (5), id INT)
 RETURNS VARCHAR (100)
 READS SQL DATA
@@ -6,3 +8,5 @@ BEGIN
 	FROM consulta c JOIN paciente p ON (c.id_paciente = p.id_paciente) JOIN cliente l ON (p.id_cliente = l.id_cliente)
 	WHERE data_consulta = data and hora_consulta = hora and id_mv = id);
 END 
+
+-- Exemplo SELECT faturamento();
